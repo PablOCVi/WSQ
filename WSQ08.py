e5c40901d8cad1ad8ece53be6a055b1d6a1e2d8e
PablOCVi <a01630814@itesm.mx>
2015-03-05 20:41:08 -0600

    Funciones

--- WSQ08.py
+++ WSQ08.py
@@ -1,12 +1,32 @@
 #print("Fun with numbers 2 (Pro Version)")
+#funcion
+def la_suma(x,y):
+	respuesta=x+y
+	return respuesta
+def la_resta(x,y):
+	respuesta=x-y
+	return respuesta
+def la_multiplicacion(x,y):
+	respuesta=x*y
+	return respuesta
+def la_Division_de_enteros(x,y):
+	respuesta=x//y
+	return respuesta
+def el_modulo(x,y):
+	respuesta=x%y
+	return respuesta
+#programa
+print("Si ingresas dos numeros el programa desplegara una serie de operaciones.")
 x=int(input("Ingresa el primer valor: "))
 y=int(input("Ingresa el segundo valor: "))
-operaciones=[]
-operaciones.append("La suma es: "+str(x+y))
-operaciones.append("La resta es: "+str(x-y))
-operaciones.append("La division en enteros es: "+str(x//y))
-operaciones.append("La division en floats es: "+str(x/y))
-operaciones.append("La multiplicacion es: "+str(x*y))
-operaciones.append("El modulo es: "+str(x%y))
-for i in range(-1,-6,-1):
-	print(operaciones[i])	
+print("los numeros dados son: "+str(x)+" , "+str(y))
+Sum=la_suma(x,y)
+print("El resultado de: "+str(x)+" + "+str(y)+" es: "+str(Sum))
+Rest=la_resta(x,y)
+print("El resultado de: "+str(x)+" - "+str(y)+" es: "+str(Rest))
+Multi=la_multiplicacion(x,y)
+print("El resultado de: "+str(x)+" * "+str(y)+" es: "+str(Multi))
+Divi=la_Division_de_enteros(x,y)
+print("El resultado de: "+str(x)+" // "+str(y)+" es: "+str(Divi))
+Mod=el_modulo(x,y)
+print("El resultado de: "+str(x)+" % "+str(y)+" es: "+str(Mod))
