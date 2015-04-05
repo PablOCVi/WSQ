d34d4c4b2b9428b32fd03bc8ae3538f92e1c765e
PablOCVi <a01630814@itesm.mx>
2015-04-05 00:03:24 -0600

    Definitive version

--- WSQ09.py
+++ WSQ09.py
@@ -8,14 +8,13 @@ def factorial(x):
 	return respuesta
 #code
 print("Si ingresas un numero positivo, desplegare el factorial del mismo.")
-y="n"
-n="y"
+n="n"
 z=""
-while (z!=y):
+while (z!=n):
 	x=int(input("Ingresa un numero positivo: "))
 	Fac=factorial(x)
 	if (x>0 or x==0):
 		print("El factorial de: "+str(x)+" es: "+str(Fac))
-		z=str(input("Necesitas el factorial de otro numero, presiona cualqioer tecla, si no presiona n.): "))
+		z=str(input("Necesitas el factorial de otro numero, presiona cualqioer tecla, si no,ingresa n.): "))
 print("Adios!")
 	
